067852d029a91a95f984adef67392c404d7f2a3d
Muhammet Tarik OZTOPRAK <tarik.oztoprak@hotmail.com>
2025-04-13 21:33:25 +0300

    Fix: Resolve SQL syntax error in Hibernate entity mappings for MySQL compatibility

--- server/noteapp/noteapp/src/main/java/com/takomak/noteapp/entities/Folder.java
+++ server/noteapp/noteapp/src/main/java/com/takomak/noteapp/entities/Folder.java
@@ -1,21 +1,16 @@
 package com.takomak.noteapp.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
 @Entity
-@Table(name="FOLDER")
+@Table(name="folder")
 @Data
 public class Folder {
     @Id
-    @Column(columnDefinition = "FOLDER_ID")
     Long folderId = 0L;
 
-    @Column(columnDefinition = "FOLDER_NAME")
     String folderName;
 
 
--- server/noteapp/noteapp/src/main/java/com/takomak/noteapp/entities/Note.java
+++ server/noteapp/noteapp/src/main/java/com/takomak/noteapp/entities/Note.java
@@ -1,35 +1,26 @@
 package com.takomak.noteapp.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="NOTE")
 @Data
+@Table(name="note")
 public class Note {
 
     @Id
-    @Column(columnDefinition = "NOTE_ID")
     Long noteId = 0L;
 
-    @Column(columnDefinition = "NOTE_TITLE")
     String noteTitle;
 
-    @Column(columnDefinition = "NOTE_CONTENT")
     String noteContent;
 
-    @Column(columnDefinition = "NOTE_DATE")
     Date noteDate;
 
-    @Column(name="NOTE_CATEGORY_ID")
     Long noteCategoryId;
 
-    @Column(columnDefinition = "NOTE_URL")
     String noteUrl;
 
 }
